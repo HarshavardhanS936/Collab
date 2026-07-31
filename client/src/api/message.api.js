118af721fbdf1d99f85295fdce32fc8afa173e37
HarshavardhanS936 <shv9306@gmail.com>
2026-07-31 18:42:29 +0530

    Fix axiosClient import in message.api.js

--- client/src/api/message.api.js
+++ client/src/api/message.api.js
@@ -1,4 +1,4 @@
-import axiosClient from './axiosClient';
+import { axiosClient } from './axiosClient';
 
 export const fetchProjectMessages = async (projectId) => {
   return axiosClient.get(`/projects/${projectId}/messages`);
